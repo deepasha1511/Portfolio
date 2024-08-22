5b496766878129c6fe68b6dcff1bf51643a23e9b
DEEPASHA <tech4dipas@gmail.com>
2024-08-23 00:16:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+ScrollReveal({
+    distance: '80px',
+    duration: 2000,
+    delay: 200,
+});
+ScrollReveal().reveal('.home-homie, heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .projects-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-homie h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-homie p, .about-whereabouts', { origin: 'right' });
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Computer Science Graduate!', 'Frontend Developer!'],
+    typeSpeed: 70,
+    backSpeed: 100,
+    loop: true,
+});
